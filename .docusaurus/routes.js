736aaca1dab1962f147c45a4836f798b6aff605b
Prakhar Gupta <prakharyologupta@gmail.com>
2024-07-10 12:49:05 +0530

    new commit

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,8 +3,43 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
-    component: ComponentCreator('/blog', 'b54'),
+    component: ComponentCreator('/blog', '782'),
     exact: true
   },
   {
@@ -20,6 +55,21 @@ export default [
   {
     path: '/blog/2024/07/08/test-document',
     component: ComponentCreator('/blog/2024/07/08/test-document', 'b2a'),
+    exact: true
+  },
+  {
+    path: '/blog/2024/07/09/hello',
+    component: ComponentCreator('/blog/2024/07/09/hello', '27f'),
+    exact: true
+  },
+  {
+    path: '/blog/2024/07/09/mhvmhmhjg',
+    component: ComponentCreator('/blog/2024/07/09/mhvmhmhjg', 'c15'),
+    exact: true
+  },
+  {
+    path: '/blog/2024/07/09/test-345',
+    component: ComponentCreator('/blog/2024/07/09/test-345', '960'),
     exact: true
   },
   {
